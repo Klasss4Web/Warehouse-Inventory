a6554f3c7be695fd01db2a56f74f3c6d794882a6
Emmanuel <eochade15@gmail.com>
2020-09-08 07:10:57 -0700

    Added a separate style sheet and created the add row button

--- index.js
+++ index.js
@@ -9,13 +9,154 @@ function Commodities(col1,col2,col3,col4,col5,col6,col7){
     }
 
 
+    //UI Elements
+let goldOp = document.getElementById('goldOpening'),
+goldInf = document.getElementById('goldInflow'),
+goldOut = document.getElementById('goldOutflow'),
+goldClos = document.getElementById('goldClosing'),
+goldAct = document.getElementById('goldActual'),
+silverOp = document.getElementById('silverOpening'),
+silverInf = document.getElementById('silverInflow'),
+silverOut = document.getElementById('silverOutflow'),
+silverClos = document.getElementById('silverClosing'),
+silverAct = document.getElementById('silverActual'),
+steelOp = document.getElementById('steelOpening'),
+steelInf = document.getElementById('steelInflow'),
+steelOut = document.getElementById('steelOutflow'),
+steelClos = document.getElementById('steelClosing'),
+steelAct = document.getElementById('steelActual'),
+mercuryOp = document.getElementById('mercuryOpening'),
+mercuryInf = document.getElementById('mercuryInflow'),
+mercuryOut = document.getElementById('mercuryOutflow'),
+mercuryClos = document.getElementById('mercuryClosing'),
+mercuryAct = document.getElementById('mercuryActual'),
+copperOp = document.getElementById('copperOpening'),
+copperInf = document.getElementById('copperInflow'),
+copperOut = document.getElementById('copperOutflow'),
+copperClos = document.getElementById('copperClosing'),
+copperAct = document.getElementById('copperActual');
+
+//Form Elements
+//Input Elements
+let itemsName = document.getElementById('item-name'),
+    inFlow = document.getElementById('in-flows'),
+    open = document.getElementById('openingStock'),
+    outFlow = document.getElementById('out-flows'),
+    closed = document.getElementById('closingStock'),
+    act = document.getElementById('actualStock'),
+    itemAdd = document.getElementById('add-item');
+
+//Items
+//let itemGold = document.getElementById('gold');
+
+//Submit Elements
+let submitItem = document.getElementById('itemSubmit'),
+    submitOpening = document.getElementById('openingSubmit'),
+    submitInflow = document.getElementById('inflowSubmit'),
+    submitOutflow = document.getElementById('outflowSubmit'),
+    submitClosing = document.getElementById('closingSubmit'),
+    submitActual = document.getElementById('actualSubmit'),
+    submitAdd = document.getElementById('addSubmit');
+
+//Add Event Listeners For the Submit Buttons
+//Opening
+submitOpening.addEventListener('click', enter);
+submitInflow.addEventListener('click', inflows);
+submitOutflow.addEventListener('click', Outflows);
 
 
-// UI Values
-addTab = document.getElementById('form-element');
+function enter(e){
+    if(itemsName.value == 'Gold'){
+        goldOp.textContent = open.value;
+    }else if(itemsName.value == 'Silver'){
+        silverOp.textContent = open.value;
+    }else if(itemsName.value == 'Steel'){
+        steelOp.textContent = open.value;
+    }else if(itemsName.value == 'Mercury'){
+        mercuryOp.textContent = open.value;
+    }else if(itemsName.value == 'Copper'){
+        copperOp.textContent = open.value;
+    }else if(open.value == "" || itemsName.value == ''){
+        errorMessage.appendChild(document.createTextNode(errorMes()));
+    }
+    setTimeout(clearError,3000)
+    
+    console.log('entering');
+    e.preventDefault();
+}
+function inflows(e){
+    if(itemsName.value == 'Gold'){
+        goldInf.textContent = inFlow.value;
+    }else if(itemsName.value == 'Silver'){
+        silverInf.textContent = inFlow.value;
+    }else if(itemsName.value == 'Steel'){
+        steelInf.textContent = inFlow.value;
+    }else if(itemsName.value == 'Mercury'){
+        mercuryInf.textContent = inFlow.value;
+    }else if(itemsName.value == 'Copper'){
+        copperInf.textContent = inFlow.value;
+    }else if(inFlow.value == "" || itemsName.value == ''){
+        errorMessage.appendChild(document.createTextNode(errorMes()))
+    } 
+    
+    setTimeout(clearError,3000)
 
-//Add Event Listener
-addTab.addEventListener('submit',function(e){
+    console.log('entering');
+    e.preventDefault();
+}
+  
+
+
+function Outflows(e){
+    if(itemsName.value == 'Gold'){
+        goldOut.textContent = outFlow.value;
+        goldClos.textContent = parseInt(goldOp.textContent) + parseInt(goldInf.textContent) - parseInt(outFlow.value); 
+    }else if(itemsName.value == 'Silver'){
+        silverOut.textContent = outFlow.value;
+        silverClos.textContent = parseInt(silverOp.textContent) + parseInt(silverInf.textContent) - parseInt(outFlow.value);
+    }else if(itemsName.value == 'Steel'){
+        steelOut.textContent = outFlow.value;
+        steelClos.textContent = parseInt(steelOp.textContent) + parseInt(steelInf.textContent) - parseInt(outFlow.value);
+    }else if(itemsName.value == 'Mercury'){
+        mercuryOut.textContent = outFlow.value;
+        mercuryClos.textContent = parseInt(mercuryOp.textContent) + parseInt(mercuryInf.textContent) - parseInt(outFlow.value);
+    }else if(itemsName.value == 'Copper'){
+        copperOut.textContent = outFlow.value;
+        copperClos.textContent = parseInt(copperOp.textContent) + parseInt(copperInf.textContent) - parseInt(outFlow.value);
+    }else if(outFlow.value == "" || itemsName.value == ''){
+        errorMessage.appendChild(document.createTextNode(errorMes()))
+    }
+   // else{
+     
+     
+     
+     console.log('enetring...');
+    //}
+    e.preventDefault();
+}
+
+let errorMessage = document.getElementById('errorMessage');
+
+function errorMes(){
+    return "please enter a valid figure"
+    
+}
+
+// Clear Error Message After a Specified Time
+function clearError(){
+    document.getElementById('errorMessage').remove();
+};
+
+
+
+// Add Tables/additional Rows
+
+addTab = document.getElementById('addTable');
+
+
+//Add Event Listener for the Add Row Button
+addTab.addEventListener('click',additionalTabs)
+    function additionalTabs(e){
 
     const serialNo = document.getElementById('serial-no').value,
         itemNames = document.getElementById('item-name').value,
@@ -26,7 +167,7 @@ addTab.addEventListener('submit',function(e){
         actual = document.getElementById('actualStock').value;
 
 
-         //instatiate commodity
+         //instatiate commodities Object
          const newAddition = new Commodities(serialNo,itemNames,opening,inflow,outflow,closing,actual);
          console.log(newAddition);
 
@@ -38,15 +179,18 @@ addTab.addEventListener('submit',function(e){
     let newTab = document.createElement('tr');
     newTab.className = 'itemRow';
 
-    newTab.innerHTML = `<th>${serialNo}</th><th>${itemNames}</th><th>${opening}</th><th>${inflow}</th><th>${outflow}</th><th>${closing}</th><th>${actual}</th>`
+    newTab.innerHTML = `<th class="serialClass">${serialNo}</th><th class="itemsname">${itemNames}</th><th>${opening}</th><th>${inflow}</th><th>${outflow}</th><th>${closing}</th><th>${actual}</th>`
+    
     console.log(newTab); 
     tables.appendChild(newTab);
-    
-    
+
+   
 
     let th = document.querySelector('th');
     
-    console.log(th)
-e.preventDefault()
+e.preventDefault();
 }
-);
+;
+
+
+
